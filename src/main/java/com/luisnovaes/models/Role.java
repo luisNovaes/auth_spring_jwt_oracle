362545fff50f9a29a7ec19958c0082f8c2cc2886
Luis Magno Da Silva Novaes <lmnovaes@stefanini.com>
2021-06-22 17:58:26 -0300

    first commit

---
+++ src/main/java/com/luisnovaes/models/Role.java
@@ -0,0 +1,48 @@
+package com.luisnovaes.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ROL_SEQ")
+	@SequenceGenerator(sequenceName = "roles_seq", allocationSize = 1, name = "ROL_SEQ")
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
+
+	public Role() {
+
+	}
+
+	public Role(ERole name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public ERole getName() {
+		return name;
+	}
+
+	public void setName(ERole name) {
+		this.name = name;
+	}
+}
